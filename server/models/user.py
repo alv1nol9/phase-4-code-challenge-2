afef213e3801c056c0ba9b41ac35aa6f9cb2f0dc
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-06-26 16:04:51 +0300

    models

---
+++ server/models/user.py
@@ -0,0 +1,13 @@
+from server.extensions import db
+from server.models.guest import Guest
+
+
+class Guest(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+
+    appearances = db.relationship('Appearance', backref='guest', cascade='all, delete-orphan')
+
